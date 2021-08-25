49518b98fb68250caa8480ae8b20debc78520b4e
YuktaJadhav <89534968+YuktaJadhav@users.noreply.github.com>
2021-08-25 21:12:05 +0530

    Add files via upload
    Code Uploaded

---
+++ README.md
@@ -0,0 +1,8 @@
+### Worldwide Live Covid Statistics
+
+#### Steps to run:
+* Clone the project using : `git clone <url>`
+* Go to directory and open the terminal routing to the same.
+* Run `npm start` and view the project hosted on `localhost: 3000`.
+<hr>
+Reach me at: @ysridhar2000 - Sridhar Y.
---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import image from "./image.png";
+import "./App.css";
+
+function App() {
+  const [cases, setCases] = useState("");
+  const [recovered, setRecovered] = useState("");
+  const [dead, setDead] = useState("");
+  const [updated, setUpdate] = useState("");
+  const [source, setSource] = useState("");
+  const [rate, setRate] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise"
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setCases(result.data.total.confirmed);
+        setRecovered(result.data.total.recovered);
+        setDead(result.data.total.deaths);
+        setUpdate(result.data.lastRefreshed);
+        setSource(result.data.source);
+        setRate(result.data.total.active);
+      });
+  });
+
+  return (
+    <div className="App">
+      <img src={image} width="100" height="100" />
+      <h1>India :: Covid-19 Statistics</h1>
+      <div className="actualStuff">
+        <div className="card">
+          <label style={{ color: "blue" }}>Source</label>
+          <p>{source}</p>
+        </div>
+        <div className="card">
+          <label style={{ color: "blue" }}>Last Updated on</label>
+          <p>{updated}</p>
+        </div>
+        <div className="card">
+          <label style={{ color: "blue" }}>Total Cases</label>
+          <p>{cases}</p>
+        </div>
+      </div>
+
+      <div className="App_t">
+        <div className="actualStuff">
+          <div className="card">
+            <label style={{ color: "blue" }}>Active</label>
+            <p>{rate}</p>
+          </div>
+
+          <div className="card">
+            <label style={{ color: "blue" }}>Recovered</label>
+            <p>{recovered}</p>
+          </div>
+          <div className="card">
+            <label style={{ color: "blue" }}>Deaths</label>
+            <p>{dead}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
